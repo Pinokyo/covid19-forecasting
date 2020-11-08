8c878978c04ba509667b34154f92b6ddab634b5d
Esin Meşeli <32396516+Pinokyo@users.noreply.github.com>
2020-11-08 20:52:49 +0300

    Add files via upload

---
+++ global-deaths-heatmap.py
@@ -0,0 +1,73 @@
+# Global Deaths Heat Map
+
+## DATA READING
+
+df_deaths = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
+df_covid19 = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/web-data/data/cases_country.csv")
+df_confirmed = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
+
+## PRE-PROCESSING
+
+df_confirmed = df_confirmed.rename(columns={"Province/State":"state","Country/Region": "country"})
+df_covid19 = df_covid19.drop(["People_Tested","People_Hospitalized","UID","ISO3","Mortality_Rate"],axis =1)
+df_deaths = df_deaths.rename(columns={"Province/State":"state","Country/Region": "country"})
+df_covid19 = df_covid19.rename(columns={"Country_Region": "country"})
+df_covid19["Active"] = df_covid19["Confirmed"]-df_covid19["Recovered"]-df_covid19["Deaths"]
+
+# Changing the conuntry names as required by pycountry_convert Lib
+df_deaths.loc[df_deaths['country'] == "US", "country"] = "USA"
+df_deaths.loc[df_deaths['country'] == 'Korea, South', "country"] = 'South Korea'
+df_deaths.loc[df_deaths['country'] == 'Taiwan*', "country"] = 'Taiwan'
+df_deaths.loc[df_deaths['country'] == 'Congo (Kinshasa)', "country"] = 'Democratic Republic of the Congo'
+df_deaths.loc[df_deaths['country'] == "Cote d'Ivoire", "country"] = "Côte d'Ivoire"
+df_deaths.loc[df_deaths['country'] == "Reunion", "country"] = "Réunion"
+df_deaths.loc[df_deaths['country'] == 'Congo (Brazzaville)', "country"] = 'Republic of the Congo'
+df_deaths.loc[df_deaths['country'] == 'Bahamas, The', "country"] = 'Bahamas'
+df_deaths.loc[df_deaths['country'] == 'Gambia, The', "country"] = 'Gambia'
+
+countries = np.asarray(df_confirmed["country"])
+countries1 = np.asarray(df_covid19["country"])
+# Continent_code to Continent_names
+continents = {
+    'NA': 'North America',
+    'SA': 'South America',
+    'AS': 'Asia',
+    'OC': 'Australia',
+    'AF': 'Africa',
+    'EU' : 'Europe',
+    'na' : 'Others'}
+
+
+# Defininng Function for getting continent code for country.
+def country_to_continent_code(country):
+    try:
+        return pc.country_alpha2_to_continent_code(pc.country_name_to_country_alpha2(country))
+    except :
+        return 'na'
+
+#Collecting Continent Information
+df_deaths.insert(2,"continent",  [continents[country_to_continent_code(country)] for country in countries[:]])
+df_covid19.insert(1,"continent",  [continents[country_to_continent_code(country)] for country in countries1[:]])
+
+df_deaths[df_deaths["continent" ]== 'Others']
+df_deaths = df_deaths.replace(np.nan, '', regex=True)
+
+df_countries_cases = df_covid19.copy().drop(['Lat','Long_','continent','Last_Update'],axis =1)
+df_countries_cases.index = df_countries_cases["country"]
+df_countries_cases = df_countries_cases.drop(['country'],axis=1)
+
+df_countries_cases.fillna(0,inplace=True)
+
+## VISUALIZATION
+
+temp_df = pd.DataFrame(df_countries_cases['Deaths'])
+temp_df = temp_df.reset_index()
+fig = px.choropleth(temp_df, locations="country",
+                    color=np.log10(temp_df["Deaths"]+1),
+                    hover_name="country",
+                    hover_data=["Deaths"],
+                    color_continuous_scale=px.colors.sequential.Plasma,locationmode="country names")
+fig.update_geos(fitbounds="locations", visible=False)
+fig.update_coloraxes(colorbar_title="Deaths (Log Scale)",colorscale="Reds")
+
+fig.show()
